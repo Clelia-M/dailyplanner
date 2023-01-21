7c05a4cccfdf9c22048e35025cce4487d365c2b7
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-21 11:38:38 +0000

    array to hold hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ moment().format();
 // Get the current day and display it at the top of the page
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").html(currentDay);
+
+// Create an array to hold the standard business hours (9AM to 5PM)
+var businessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+for (var i = 0; i < businessHours.length; i++) {
+    var hour = businessHours[i];
+    var hourIn12Format = moment(hour, "hh A").format("hh A");
+    businessHours[i] = hourIn12Format;
+}
